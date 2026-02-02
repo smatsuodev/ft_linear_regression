42467c73a81f4bf25cc6b465e0c86c2d2a3a39f0
smatsuodev <git@smatsuo.dev>
2026-02-02 22:38:36 +0900

    モデルのパスを指定できるオプションを追加

--- predict.py
+++ predict.py
@@ -1,17 +1,28 @@
+import sys
+
+
 def main():
+    args = sys.argv
+
+    if len(args) not in [1, 2]:
+        print("Usage: python predict.py [<model_file>]")
+        return 1
+
+    model_path = args[1] if len(args) == 2 else ".model"
+
     try:
-        with open(".model") as f:
-            theta0,theta1 = map(float,f.read().split(','))
+        with open(model_path) as f:
+            theta0, theta1 = map(float, f.read().split(","))
 
     except FileNotFoundError:
         print("Model file not found.")
         return 1
-    
+
     except ValueError:
         print("Invalid model.")
         return 1
 
-    try:   
+    try:
         mileage = float(input("mileage: "))
     except ValueError:
         print("Invalid mileage.")
